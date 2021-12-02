89b6ff72d94cef931f8cd12338286da31090d061
moritamu <moris@r.sannet.ne.jp>
2021-12-03 07:48:36 +0900

    Initial Commit

---
+++ MyTimer1/MyTimer1App.swift
@@ -0,0 +1,17 @@
+//
+//  MyTimer1App.swift
+//  MyTimer1
+//
+//  Created by 森田晋 on 2021/12/03.
+//
+
+import SwiftUI
+
+@main
+struct MyTimer1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
